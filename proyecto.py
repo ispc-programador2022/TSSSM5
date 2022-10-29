9c3833b23c24ef47895e48f609d4ffe38f66d6d0
leomatias <lmatias.mansilla@hotmail.com.ar>
2022-10-29 19:05:28 -0300

    inicio

--- proyecto.py
+++ proyecto.py
@@ -1,11 +1,3 @@
 # TSSSM5 Inicio de proyecto - Generación y Analisis de información mediante WEB SCRAPING
 # Análisis de los movimientos migratorios. 
 
-
-
-# Se importan las librerias necesarias. 
-from bs4 import BeautifulSoup as bs
-import requests as rqst
-import pandas as pd
-import numpy as np
-import re
