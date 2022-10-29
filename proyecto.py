da58fd3cbda6edfd98ca3f49f7775769eb75c879
leomatias <lmatias.mansilla@hotmail.com.ar>
2022-10-29 19:03:08 -0300

    se da inicio al proyecto WEB SCRAPING utilizando metodologias ágiles

---
+++ proyecto.py
@@ -0,0 +1,11 @@
+# TSSSM5 Inicio de proyecto - Generación y Analisis de información mediante WEB SCRAPING
+# Análisis de los movimientos migratorios. 
+
+
+
+# Se importan las librerias necesarias. 
+from bs4 import BeautifulSoup as bs
+import requests as rqst
+import pandas as pd
+import numpy as np
+import re
